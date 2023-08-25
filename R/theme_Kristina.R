ecb45b148c0650c506188dd6a2042b8f01cc512d
kristinaglass <kristinaglass48@gmail.com>
2023-08-25 11:09:17 -0700

    made new theme

---
+++ R/theme_Kristina.R
@@ -0,0 +1,12 @@
+#' this is a theme made by kristina :3
+#'
+#' @return
+#' @export
+#'
+#' @examples
+theme_kristina <- function() {
+  theme(plot.background = element_rect(fill = "goldenrod"),
+        panel.background = element_rect(fill = "lightsalmon3"),
+        axis.text = element_text(color = "darkolivegreen"),
+        panel,grid = element_line(color = "coral4"))
+}
