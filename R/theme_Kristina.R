60ed591fb32a79191f5a2ce65f0cf764b65197ed
kristinaglass <kristinaglass48@gmail.com>
2023-08-25 11:27:42 -0700

    added panel back

--- R/theme_Kristina.R
+++ R/theme_Kristina.R
@@ -6,6 +6,7 @@
 #' @examples
 theme_kristina <- function() {
   theme(plot.background = element_rect(fill = "goldenrod"),
+        panel.background = element_rect(fill = "lightsalmon1"),
         axis.text = element_text(color = "darkolivegreen"),
         panel.grid = element_line(color = "coral4"))
 }
